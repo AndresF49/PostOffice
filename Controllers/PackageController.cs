c1266a498e6900b7a5a645280bf5f77cd29dcbb4
Andres F <andres06037@gmail.com>
2023-11-01 11:30:17 -0500

    added tracking numbers

--- Controllers/PackageController.cs
+++ Controllers/PackageController.cs
@@ -9,6 +9,7 @@ public class PackageController : ControllerBase
 
 	private static IEnumerable<Package> Packages = new[] {
 		new Package { PackageId=1, Status="Pending", Sender="Andres", 
+						TrackingNumber="12345",
 						SourceAddress="123 Sesame Street Houston, Tx", Receiver="Stephen", 
 						DestinationAddress="456 Hillsboro Blvd Corpus Christi, Tx", Price=25.34,
 						DescriptionOfItem="Electronics", DeclaredValue=125.00, PackageType="Package",
@@ -16,13 +17,15 @@ public class PackageController : ControllerBase
 						Insurance=true
 					},
 		new Package { PackageId=2, Status="Shipped", Sender="Stephen", 
+						TrackingNumber="55555",
 						SourceAddress="789 Cullen Blvd Houston, Tx", Receiver="Andres", 
 						DestinationAddress="1212 Generic Crt Bellaire, Tx", Price=10.88,
 						DescriptionOfItem=null, DeclaredValue=null, PackageType="Envelope",
 						Weight=null, Length=null, Width=null, Depth=null, SignatureRequired=false,
 						Insurance=false
 					},
-		new Package { PackageId=3, Status="Delivered", Sender="Elmo", 
+		new Package { PackageId=3, Status="Delivered", Sender="Elmo",
+						TrackingNumber="ABC123", 
 						SourceAddress="112 Sesame St Houston, Tx", Receiver="Oscar", 
 						DestinationAddress="357 Forrest Dr Spring, Tx", Price=28.45,
 						DescriptionOfItem="Nunya", DeclaredValue=250, PackageType="Package",
@@ -47,17 +50,24 @@ public class PackageController : ControllerBase
 		
     }
 
-	// [HttpPost]
-	// public IActionResult PostToDoItem(ToDoItem toDoItem) 
-	// {
-	// 	// supposed to be an async/await because we will be waiting for response from DB
+	[HttpPost]
+	[Route("SearchPackage")]
+        public ActionResult<Package> SearchPackage(string searchRequest)
+        {
+            if (string.IsNullOrEmpty(searchRequest))
+            {
+                return BadRequest("Invalid search request.");
+            }
 
-	// 	// DB SQL query or sumthin
+			var searchResult = Packages.Where(x => x.TrackingNumber == searchRequest);
 
-	// 	ListItems.Add(toDoItem);
+			if (searchResult == null)
+            {
+                return NotFound("Object not found.");
+            }
 
-	// 	return Ok();
+            return Ok(searchResult);
+        }
 
-	// }
 }
 
