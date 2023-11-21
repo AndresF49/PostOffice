9548d06fdc04f7883de2c0570143af4e4a4c9752
Andres F <andres06037@gmail.com>
2023-11-20 19:04:07 -0600

    login switched to functional comp

--- ClientApp/src/components/Account/Login.js
+++ ClientApp/src/components/Account/Login.js
@@ -1,18 +1,12 @@
 ﻿import React, { Component } from 'react';
 import LoginForm from './LoginForm';
 
-export class Login extends Component {
-    static displayName = Login.name;
+export default function Login({ setToken }) {
 
-    // const { register } = useForm();
-
-    // lets do some validation on the fields too!
-    // we may be able to do input fields as reusable components
-    render() {
         return (
             <>
-                <LoginForm />
+                <LoginForm setToken={setToken}/>
             </>
         );
-    }
+    // }
 }
