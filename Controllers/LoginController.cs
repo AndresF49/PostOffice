e57a23ab1dd5ac08add35f1c7fea35886ec8e694
Andres F <andres06037@gmail.com>
2023-11-21 15:41:54 -0600

    added model folder and userArr for testing

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -23,8 +23,25 @@ public class LoginController : ControllerBase
 	public class Token // returning this token object so frontend destructure this object into a token and email
 	{
 		public string token { get; set; }
-		public string email { get; set; }
+		public User user { get; set; }
 	}
+
+	// private readonly int[] _foo = new int[] {0,1,2,3};
+	private readonly User[] UserArr = new User[] 
+	{ 
+		new User { Userid=0, Email="admin@gmail.com", RoleTypeId=0, EmployeeId=0, Username="admin", Password="admin", CustomerId=null },
+	 	new User { Userid=1, Email="andres@gmail.com", RoleTypeId=0, EmployeeId=1, Username="andres", Password="andres", CustomerId=null },
+		new User { Userid=2, Email="employee@gmail.com", RoleTypeId=1, EmployeeId=2, Username="employee", Password="employee", CustomerId=null },
+		new User { Userid=3, Email="customer@gmail.com", RoleTypeId=2, EmployeeId=null, Username="customer", Password="customer", CustomerId=0 }
+	};
+
+	public enum Roles 
+	{
+		Admin=0,
+		Employee=1,
+		Customer=2
+	}
+	
 	[HttpPost]
 	[Route("Login")]
 	public async Task<IActionResult> Login([FromBody] LoginCredentials credentials) // By default, Web API tries to get simple types from the request URI. The FromBody attribute tells Web API to read the value from the request body.
@@ -34,10 +51,13 @@ public class LoginController : ControllerBase
 			Console.WriteLine("email or pass null/emp");
 			return BadRequest("One or more Login credentials is empty");
 		}
-
 		
 		await Task.Run(() => Thread.Sleep(100));
-		var tokenObj = new Token { token = "test123", email = credentials.email };
+		// query db where credentials.email == db.email && Hash(credentials.password) == db.hashedPassword
+		var user = UserArr.Single<User>(u => u.Email == credentials.email && u.Password == credentials.password);
+		// NEEDS TO BE HASHED PASSWORD ^^^^ and SWITCH frontend to take a Username when logging in/registering instead
+		// of an email
+		var tokenObj = new Token { token = "test123", user = user };
   //      Console.WriteLine("In Post method for login:");
   //      Console.WriteLine("Email: " + credentials.email);
   //      Console.WriteLine("Pass: " + credentials.password);
---
+++ Models/User.cs
@@ -0,0 +1,10 @@
+public class User 
+	{
+		public int Userid { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
+		public int RoleTypeId { get; set; }
+		public string Email { get; set; }
+		public int? CustomerId { get; set; }
+		public int? EmployeeId { get; set; }
+	}
