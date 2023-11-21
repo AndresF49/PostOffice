f4c1849d5b5e94ba23cc613177787078eaea7ab9
Andres F <andres06037@gmail.com>
2023-11-20 20:32:11 -0600

    conditionally renders app if authenticated or not.

--- ClientApp/src/App.js
+++ ClientApp/src/App.js
@@ -1,19 +1,59 @@
 import React from 'react';
-import { Route, Routes } from 'react-router-dom';
+import { Navigate, Route, Routes } from 'react-router-dom';
 import AppRoutes from './AppRoutes';
-import { Layout } from './components/Layout';
+import Layout from './components/Layout';
 import './custom.css';
+import useToken from './components/Utils/useToken';
+import Login from "./components/Account/Login";
+import { Register } from './components/Account/Register';
+import ErrorPage from './components/ErrorPage';
+import { useState } from 'react';
+import { useEffect } from 'react';
+
 
 export default function App() {
 
+  const { token, setToken } = useToken();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+    console.log("isAuthenticated now: " + isAuthenticated);
+  }, [isAuthenticated]);
+
+  // if(!token) {
+  //   return (
+  //     (!isAuthenticated && 
+  //     <Routes>
+  //       <Route key={0} index={true} path={"/login"} element={<Login setToken={setToken} setIsAuthenticated={setIsAuthenticated} />} errorElement={<ErrorPage />} />
+  //       <Route key={1} path={"/register"} element={<Register />} errorElement={<ErrorPage />}/>
+  //       <Route key={2} path={"*"} element={<Navigate to="/login" />} errorElement={<ErrorPage />}/>
+  //     </Routes>
+  //     )
+  //   );
+  // }
+
+  const notAuthenticatedRoutes = () => {
+    return (
+    <Routes>
+      <Route key={0} index={true} path={"/login"} element={<Login setToken={setToken} setIsAuthenticated={setIsAuthenticated} />} errorElement={<ErrorPage />} />
+      <Route key={1} path={"/register"} element={<Register />} errorElement={<ErrorPage />}/>
+      <Route key={2} path={"*"} element={<Navigate to="/login" />} errorElement={<ErrorPage />}/>
+    </Routes>
+    );
+  }
+
   return (
-    <Layout>
-      <Routes>
-        {AppRoutes.map((route, index) => {
-          const { element, ...rest } = route;
-          return <Route key={index} {...rest} element={element} />;
-        })}
-      </Routes>
-    </Layout>
+    (isAuthenticated ? 
+      <Layout setIsAuthenticated={setIsAuthenticated}>
+        <Routes>
+          {AppRoutes.map((route, index) => {
+            const { element, ...rest } = route;
+            return <Route key={index} {...rest} element={element} />;
+          })}
+        </Routes>
+      </Layout> 
+      :
+      notAuthenticatedRoutes()
+    )
   );
 }
