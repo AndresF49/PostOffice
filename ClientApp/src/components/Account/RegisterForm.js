6248928ec99edba113bba35f9124e192ef393c94
Andres F <andres06037@gmail.com>
2023-11-23 11:48:49 -0600

    registration in progress

--- ClientApp/src/components/Account/RegisterForm.js
+++ ClientApp/src/components/Account/RegisterForm.js
@@ -1,194 +1,266 @@
 import { Button, Form, Label, FormGroup } from "reactstrap";
 import { useForm } from 'react-hook-form'
 import { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Roles } from "./Roles";
 
 // do not use Input component from reactstrap for form validation with useForm :(
 
-// async function RegisterUser(credentials) {
+async function CreateUser(credentials, roleTypeNum) {
 
-// 	try {
-// 	  const response = await fetch('register/Register', {
-// 		method: 'POST',
-// 		headers: {
-// 		'Content-Type': 'application/json'
-// 		},
-// 		body: JSON.stringify({ username: credentials.username, password: credentials.password }) // explicitly state each parameter, both this line and line below work tho >:(
-// 		// body: JSON.stringify( credentials )
-// 	  });
-// 	  // console.log("response: ", response);
-  
-// 	  const result = await response.json();
-// 	  return result;
-// 	} catch (error) {
-// 	  // console.log("error in loginUser with creds: ", credentials);
-// 	  console.log("error: ", error);
-// 	}
-//   }
+  try {
+    const response = await fetch('register/CreateUser', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        Email: credentials.Email,
+        Username: credentials.Username,
+        Password: credentials.Password,
+        RoleTypeId: Roles[roleTypeNum],
+      }) 
+    }); 
 
-// access CustomersArray CustomerArr and add to it when registering, also add to UsersArray -> UserArr as well
+    const result = response.json();
+    return result;
+  } catch (error) {
+    console.log("Error when creating User: ", error);
+  }
+}
+
+async function CreateCustomer(credentials) {
+  // public int CustomerId { get; set; }
+  // public string FirstName { get; set; }
+  // public char? MiddleInitial { get; set; }
+  // public string LastName { get; set; }
+  // public string? PhoneNumber { get; set; }
+  // public string Email { get; set; }
+  // public int? BillingAddressId { get; set;}
+  // public User User { get; set; }
+  try {
+    const response = await fetch('register/CreateCustomer', {
+      method: 'POST',
+      headers: {
+      'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ 
+        FirstName: credentials.FirstName,
+        MiddleInitial: credentials.MiddleInitial ? credentials.MiddleInitial : null,
+        LastName: credentials.LastName,
+        PhoneNumber: credentials.PhoneNumber ? credentials.PhoneNumber : null,
+        Email: credentials.Email,
+      }) // explicitly state each parameter, both this line and line below work tho >:(
+      // body: JSON.stringify( credentials )
+    });
+    // console.log("response: ", response);
+
+    const result = await response.json();
+    return result;
+  } catch (error) {
+    // console.log("error in loginUser with creds: ", credentials);
+    console.log("Error when creating Customer: ", error);
+  }
+}
+
+  // access CustomersArray CustomerArr and add to it when registering, also add to UsersArray -> UserArr as well
 
 
-export default function RegisterForm() {
-	const { register, watch, handleSubmit, formState } = useForm();
-	const { errors } = formState
-	const [userInfo, setUserInfo] = useState();
-	// const watchConfirmPassword = watch("confirmPassword", ''); // you can supply default value as second argument
+export default function RegisterForm({ setAuthentication }) {
+  const { register, watch, handleSubmit, formState } = useForm();
+  const { errors } = formState
+  const navigate = useNavigate();
 
-	
+  // const watchConfirmPassword = watch("confirmPassword", ''); // you can supply default value as second argument
 
-	const onSubmit = (data) => {
-		setUserInfo(data)
-		console.log(data);
-	};
+  const onSubmit = async (credentials) => {
+    const newUserId = await CreateUser(credentials, 2);
+    credentials.UserId = newUserId;
 
-	return (
-		<div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
-			<div className="card border-0 shadow rounded-3 my-5">
-				<div className="card-body p-4 p-sm-5">
-					<h5 className="card-title text-center mb-5 fw-light fs-2">Register</h5>
-					{/* <pre>{JSON.stringify(userInfo, undefined, 2)}</pre> */}
-					<Form noValidate onSubmit={ handleSubmit(onSubmit) } >
-						<FormGroup floating className="mb-3">
-							<input 
-								type="text" 
-								className="form-control" 
-								id="FirstName" 
-								placeholder="First name"
-								{...register("FirstName", {
-									required: {
-										value: true,
-										message: 'First name is required'
-									},
-								})}
-							/>
-							<Label htmlFor="FirstName">
-								First name
-							</Label>
-							<p className="text-danger mt-1">{errors.FirstName?.message}</p>
+    const authDetails = await CreateCustomer(credentials);
 
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-							<input 
-								type="text" 
-								className="form-control" 
-								id="MiddleInitial" 
-								placeholder="Middle initial"
-								maxLength="1"
-								{...register("MiddleInitial", {
-									maxLength: 1
-								})}
-							/>
-							<Label htmlFor="MiddleInitial">
-								Middle initial
-							</Label>
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-							<input 
-								type="text" 
-								className="form-control" 
-								id="LastName" 
-								placeholder="Last name"
-								{...register("LastName", {
-									required: {
-										value: true,
-										message: 'Last name is required'
-									},
-								})}
-							/>
-							<Label htmlFor="LastName">
-								Last name
-							</Label>
-							<p className="text-danger mt-1">{errors.LastName?.message}</p>
+    setAuthentication({
+      currentUser: authDetails.user,
+      role: Roles[authDetails.user.RoleTypeId],
+      token: authDetails.token
+    });
+    // console.log("Info below from token=loginUser")
+    // console.log(`User: ${token.user}`);
+    // console.log(`Role: ${Roles[token.user.RoleTypeId]}`);
+    // console.log(`Token: ${token.token}`);
+    navigate("/");
+  };
 
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-							<input 
-								type="text" 
-								className="form-control" 
-								id="PhoneNumber" 
-								placeholder="Phone number"
-								{...register("PhoneNumber")}
-							/>
-							<Label htmlFor="PhoneNumber">
-								Phone Number
-							</Label>
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-							<input 
-								type="email" 
-								className="form-control" 
-								id="email" 
-								placeholder="name@example.com"
-								{...register("Email", {
-									required: {
-										value: true,
-										message: 'Email is required'
-									},
-									pattern: {
-										value: /[^\s@]+@[^\s@]+\.[^\s@]+/,
-										message: 'Invalid email format'
-									}
-								})}
-							/>
-							<Label htmlFor="email">
-								Email address
-							</Label>
-							<p className="text-danger mt-1">{errors.Email?.message}</p>
+  // const onSubmit = (data) => {
+  // 	setUserInfo(data)
+  // 	console.log(data);
+  // };
 
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-						<input 
-							type="password" 
-							className="form-control" 
-							id="password" 
-							placeholder="Password"
-							{...register("Password", {
-								required: {
-									value: true,
-									message: 'Password is required'
-								},
-							})}
-						/>
-						<Label htmlFor="password">
-							Password
-						</Label>
-						<p className="text-danger mt-1">{errors.Password?.message}</p>
+  return (
+    <div className="col-sm-9 col-md-7 col-lg-5 mx-auto">
+      <div className="card border-0 shadow rounded-3 my-5">
+        <div className="card-body p-4 p-sm-5">
+          <h5 className="card-title text-center mb-5 fw-light fs-2">Register</h5>
+          <Form noValidate onSubmit={ handleSubmit(onSubmit) } >
+            <FormGroup floating className="mb-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="FirstName" 
+                placeholder="First name"
+                {...register("FirstName", {
+                  required: {
+                    value: true,
+                    message: 'First name is required'
+                  },
+                })}
+              />
+              <Label htmlFor="FirstName">
+                First name
+              </Label>
+              <p className="text-danger mt-1">{errors.FirstName?.message}</p>
 
-						</FormGroup>
-						<FormGroup floating className="mb-3">
-							<input 
-								type="password" 
-								className="form-control" 
-								id="confirmPassword" 
-								placeholder="Password"
-								{...register("ConfirmPassword", {
-									required: {
-										value: true,
-										message: 'Confirm password is required'
-									},
-									validate: (val) => {
-										if (watch('Password') != val) {
-										  return "Your passwords do no match";
-										}
-									}
-								})}
-							/>
-							<Label htmlFor="confirmPassword">
-								Confirm password
-							</Label>
-							<p className="text-danger mt-1">{errors.ConfirmPassword?.message}</p>
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="MiddleInitial" 
+                placeholder="Middle initial"
+                maxLength="1"
+                {...register("MiddleInitial", {
+                  maxLength: 1
+                })}
+              />
+              <Label htmlFor="MiddleInitial">
+                Middle initial
+              </Label>
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="LastName" 
+                placeholder="Last name"
+                {...register("LastName", {
+                  required: {
+                    value: true,
+                    message: 'Last name is required'
+                  },
+                })}
+              />
+              <Label htmlFor="LastName">
+                Last name
+              </Label>
+              <p className="text-danger mt-1">{errors.LastName?.message}</p>
 
-						</FormGroup>
-						<p className="text-left">Already have an account? <Link to="/login">Login</Link></p>
-						<div className="d-grid">
-							<Button className="btn-primary btn-login text-uppercase fw-bold" type="submit" >
-								Register
-							</Button>
-						</div>
-					</Form>
-				</div>
-			</div>
-		</div>
-	  );
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="PhoneNumber" 
+                placeholder="Phone number"
+                {...register("PhoneNumber")}
+              />
+              <Label htmlFor="PhoneNumber">
+                Phone Number
+              </Label>
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="email" 
+                className="form-control" 
+                id="email" 
+                placeholder="name@example.com"
+                {...register("Email", {
+                  required: {
+                    value: true,
+                    message: 'Email is required'
+                  },
+                  pattern: {
+                    value: /[^\s@]+@[^\s@]+\.[^\s@]+/,
+                    message: 'Invalid email format'
+                  }
+                })}
+              />
+              <Label htmlFor="email">
+                Email address
+              </Label>
+              <p className="text-danger mt-1">{errors.Email?.message}</p>
+
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="text" 
+                className="form-control" 
+                id="username" 
+                placeholder="username"
+                {...register("Username", {
+                  required: {
+                    value: true,
+                    message: 'Username is required'
+                  }
+                })}
+              />
+              <Label htmlFor="username">
+                Username
+              </Label>
+              <p className="text-danger mt-1">{errors.Username?.message}</p>
+
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+            <input 
+              type="password" 
+              className="form-control" 
+              id="password" 
+              placeholder="Password"
+              {...register("Password", {
+                required: {
+                  value: true,
+                  message: 'Password is required'
+                },
+              })}
+            />
+            <Label htmlFor="password">
+              Password
+            </Label>
+            <p className="text-danger mt-1">{errors.Password?.message}</p>
+
+            </FormGroup>
+            <FormGroup floating className="mb-3">
+              <input 
+                type="password" 
+                className="form-control" 
+                id="confirmPassword" 
+                placeholder="Password"
+                {...register("ConfirmPassword", {
+                  required: {
+                    value: true,
+                    message: 'Confirm password is required'
+                  },
+                  validate: (val) => {
+                    if (watch('Password') != val) {
+                      return "Your passwords do no match";
+                    }
+                  }
+                })}
+              />
+              <Label htmlFor="confirmPassword">
+                Confirm password
+              </Label>
+              <p className="text-danger mt-1">{errors.ConfirmPassword?.message}</p>
+
+            </FormGroup>
+            <p className="text-left">Already have an account? <Link to="/login">Login</Link></p>
+            <div className="d-grid">
+              <Button className="btn-primary btn-login text-uppercase fw-bold" type="submit" >
+                Register
+              </Button>
+            </div>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
 }
