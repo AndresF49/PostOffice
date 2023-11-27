dda1f11be6a24859989a18cc0b760ec5ea5bb048
Andres F <andres06037@gmail.com>
2023-11-27 17:19:17 -0600

    fix report

--- DataAccess/Reports/AnnualRevenueReport/GetAnnualRevenueReportResponse.cs
+++ DataAccess/Reports/AnnualRevenueReport/GetAnnualRevenueReportResponse.cs
@@ -3,8 +3,8 @@
     public class GetAnnualRevenueReportResponse
     {
         public int PostOfficeId { get; set; }
-        public int PostMasterId { get; set; }
-        public string PostMasterName { get; set;}
+        // public int PostMasterId { get; set; }
+        // public string PostMasterName { get; set;}
         public double TotalRevenue { get; set; }
     }
 }
